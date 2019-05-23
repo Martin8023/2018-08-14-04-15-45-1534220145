97c7ea72766c0b9edc31a3f2ae5029455d686e1f
martin <martin802399@outlook.com>
2019-05-23 10:35:54 +0800

    done task

--- main/main.js
+++ main/main.js
@@ -1,4 +1,9 @@
 // Write your cade below:
-module.exports = function main() {
-  
+module.exports = function main(x,y) {
+  // console.log(x+" "+y);
+  if (x == null || y == null) return -1;
+  if(y == 0) return -1;
+
+  // console.log(x%y);
+  return x%y;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -5,5 +5,8 @@ describe('main()', () => {
     it('should calculate the remaindar', () => {
         expect(main(9, 3)).toBe(0);
         expect(main(11, 4)).toBe(3);
+        // expect(main(11, 0)).toBe(-1);
+        // expect(main(11)).toBe(-1);
+        // expect(main()).toBe(-1);
     });
 });
